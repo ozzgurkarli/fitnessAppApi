159593e66e036e8b2b9953ba892ef720c7edab29
ozzgurkarli <ozzgurkarli2@gmail.com>
2024-04-14 17:39:41 +0300

    CONSTANT update

--- fitnessAppApi/Constants/Muscle.cs
+++ fitnessAppApi/Constants/Muscle.cs
@@ -2,20 +2,22 @@
 {
     public class Muscle
     {
-        public readonly static int CHEST = 0;
+        public readonly static int NONE = 0;
 
-        public readonly static int SHOULDER = 1;
+        public readonly static int CHEST = 1;
 
-        public readonly static int BACK = 2;
+        public readonly static int SHOULDER = 2;
 
-        public readonly static int BICEPS = 3;
+        public readonly static int BACK = 3;
 
-        public readonly static int TRICEPS = 4;
+        public readonly static int BICEPS = 4;
 
-        public readonly static int LEG = 5;
+        public readonly static int TRICEPS = 5;
 
-        public readonly static int ABDOMEN = 6;
+        public readonly static int LEG = 6;
 
-        public readonly static int CARDIO = 7;
+        public readonly static int ABDOMEN = 7;
+
+        public readonly static int CARDIO = 8;
     }
 }
