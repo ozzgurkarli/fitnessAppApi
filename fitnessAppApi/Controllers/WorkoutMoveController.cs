18b221b20d7843644c7bf67078692b69f436d1d9
ozzgurkarli <ozzgurkarli2@gmail.com>
2024-06-11 23:21:19 +0300

    workoutmovecontroller created

---
+++ fitnessAppApi/Controllers/WorkoutMoveController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using fitnessAppApi.DTO;
+using fitnessAppApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fitnessAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkoutMoveController(FitnessContext context, IMapper _mapper) : ControllerBase
+    {
+        private readonly FitnessContext db = context;
+        private readonly IMapper mapper = _mapper;
+
+        #region public methods
+
+
+        [HttpPut("UpdateWorkoutMove")]
+        public async Task<IActionResult> UpdateWorkoutMove(DTOWorkoutMove move)
+        {
+            WorkoutMove model = db.WorkoutMove.Where(x => x.Id == move.Id).First();
+
+            model.SetCount++;
+            model.Repeat += move.Repeat;
+            model.Weight += (move.Repeat * move.Weight);
+
+            await db.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        #endregion public methods
+    }
+}
